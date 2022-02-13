133950fe45a30295d830c7236a9b3c68b06da0e0
穿越电线 <71607036+CYDXDianXian@users.noreply.github.com>
2022-02-14 05:57:34 +0800

    Add files via upload

--- image_transcoding.py
+++ image_transcoding.py
@@ -4,12 +4,13 @@ import time
 from pathlib import Path
 
 
-source_folder = "img"
-output_folder = "output"
-img_type = input("请输入目标格式：") # input函数返回的是用户输入内容，为字符串类型，若需要计算数字，用int()或者float()函数转换数据类型
+path_input = "img"
+path_output = "output"
 
-path_input = Path(__file__).parent / source_folder
-path_output = Path(__file__).parent / output_folder
+Path(path_input).mkdir(parents = True, exist_ok = True)
+print('img文件夹已创建，请将待转换的图片放入img文件夹中')
+
+img_type = input("请输入目标格式：") # input函数返回的是用户输入内容，为字符串类型，若需要计算数字，用int()或者float()函数转换数据类型
 
 Path(path_output).mkdir(parents = True, exist_ok = True)
 
@@ -17,6 +18,7 @@ try:
     path_list = [i for i in Path(path_input).iterdir()] # 当路径指向一个目录时，产生该路径下的所有对象的路径。iterdir()返回的是一个生成器，需要循环遍历才能读取
 except FileNotFoundError:
     print('文件目录下没有img文件夹，请创建文件夹并将图片放入img文件夹中')
+    time.sleep(5) # sleep操作，防止用户看不到打印消息程序就结束了
     sys.exit()
 
 def changeType():
@@ -53,6 +55,8 @@ def changeType():
 
     print(f'全部转换完成！！！共成功{success_num}个，失败{error_num}个，用时{int(end - start)}秒')
     print(f'文件输出路径：{path_output}')
+    print('程序将在10秒后结束......')
+    time.sleep(10) # sleep操作，防止用户看不到打印消息程序就结束了
 
 if __name__ == "__main__":
     changeType() 
